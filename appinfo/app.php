eb03b5ca51549d3e88f85da5e9850959f49cf471
ggeorgg <georg@grossmann-technologies.de>
2015-11-14 23:39:02 +0100

    Update app.php

--- appinfo/app.php
+++ appinfo/app.php
@@ -12,7 +12,7 @@
 
 OCP\App::registerAdmin ( 'piwik', 'settings-admin' );
 
-$internal = OCP\Config::getAppValue ( 'piwik', 'internal','no' );
+$internal = OCP\Config::getAppValue ( 'piwik', 'internal','' );
 
 if ($internal === 'yes') {
    OCP\Util::addScript ( 'piwik', 'piwik');
