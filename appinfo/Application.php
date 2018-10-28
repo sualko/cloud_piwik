bd2b33a8ea76a7f132bde132f95984075717c9e9
sualko <klaus@jsxc.org>
2018-10-28 14:26:08 +0100

    convert to dynamic tracking script
    this saves one request and removes any client-side cache

--- appinfo/Application.php
+++ appinfo/Application.php
@@ -3,6 +3,7 @@ namespace OCA\Piwik\AppInfo;
 
 use OCA\Piwik\Config;
 use OCA\Piwik\Controller\SettingsController;
+use OCA\Piwik\Controller\JavaScriptController;
 use OCA\Piwik\Migration\Settings as SettingsMigration;
 use OCP\AppFramework\App;
 use OCP\IContainer;
@@ -28,6 +29,14 @@ class Application extends App
          */
         $container->registerService('SettingsController', function (IContainer $c) {
             return new SettingsController(
+                $c->query('AppName'),
+                $c->query('Request'),
+                $c->query('OCA\Piwik\Config')
+            );
+        });
+
+        $container->registerService('JavaScriptController', function (IContainer $c) {
+            return new JavaScriptController(
                 $c->query('AppName'),
                 $c->query('Request'),
                 $c->query('OCA\Piwik\Config')
