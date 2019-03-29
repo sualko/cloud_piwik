f32be86dd2bb228dbded33f88c78cbb5bdd8ecbc
sualko <klaus@jsxc.org>
2019-03-29 09:48:32 +0100

    fix: script loading with pretty urls
    fix #63

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -4,7 +4,7 @@ return [
 	'routes' => [
 		['name' => 'settings#index', 'url' => '/settings', 'verb' => 'GET'],
 		['name' => 'settings#update', 'url' => '/settings/{key}', 'verb' => 'PUT'],
-		['name' => 'JavaScript#tracking', 'url' => '/js/tracking.js', 'verb' => 'GET']
+		['name' => 'JavaScript#tracking', 'url' => '/js/tracking', 'verb' => 'GET']
 	],
 ];
 ?>
