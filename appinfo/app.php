fd9013ae442a07f679ae38d523b4ca5554ed80ce
sualko <klaus@jsxc.org>
2019-05-21 13:56:59 +0200

    fix: csp for NC 16
    add self to csp if matomo is running on the same host
    
    related to #30, obsolete #70

--- appinfo/app.php
+++ appinfo/app.php
@@ -10,18 +10,22 @@ if (!empty($url)) {
         ], ''
     );
 
+    $allowedUrl = ' \'self\' ';
     $parseurl = parse_url($url);
-    $url = (isset($parseurl['host'])) ? $parseurl['host'] : false;
-    if (isset($parseurl['port'])) {
-      $url .= ':' . (string) $parseurl['port'];
+
+    if (isset($parseurl['host']) && array_key_exists('HTTP_HOST', $_SERVER)
+        && $_SERVER['HTTP_HOST'] !== $parseurl['host']) {
+        $allowedUrl = $parseurl['host'];
+
+        if (isset($parseurl['port'])) {
+            $allowedUrl .= ':' . (string) $parseurl['port'];
+        }
     }
+
     $policy = new OCP\AppFramework\Http\ContentSecurityPolicy();
 
-    if ($url !== false && array_key_exists('HTTP_HOST', $_SERVER)
-        && $_SERVER['HTTP_HOST'] !== $url && !empty($url)) {
-        $policy->addAllowedScriptDomain($url);
-        $policy->addAllowedImageDomain($url);
+    $policy->addAllowedScriptDomain($allowedUrl);
+    $policy->addAllowedImageDomain($allowedUrl);
 
-        \OC::$server->getContentSecurityPolicyManager()->addDefaultPolicy($policy);
-    }
+    \OC::$server->getContentSecurityPolicyManager()->addDefaultPolicy($policy);
 }
